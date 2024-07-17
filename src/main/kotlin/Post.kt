9753a79a04d1ab93b81cbb77f0e1c8af87a3ea95
RussOne <shaparev.daniel@gmail.com>
2024-07-17 20:47:26 +0200

    fixed

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -1,20 +1,25 @@
-data class Post constructor(
+data class Post(
     var id: Int = 0,
-    val ownerId: Int = 0,
-    val text: String = "",
-    val fromId: Int = 0,
-    var likes: Int = 0
+    var ownerId: Int = 0,
+    var text: String = "",
+    var fromId: Int = 0,
+    var view: WallService.View = WallService.View(),
+    val likes: WallService.Likes = WallService.Likes()
+
 )
 
 object WallService {
 
     private var posts = emptyArray<Post>()
+    private var countWall: Int = 0
 
 
     fun add(post: Post): Post {
-        posts += post
 
-        post.id = posts.size
+        val postIdent = post.copy(countWall + 1)
+        countWall++
+        posts += postIdent
+
         return posts.last()
     }
 
@@ -28,33 +33,39 @@ object WallService {
         return false
     }
 
+    fun clear() {
+        posts = emptyArray()
+        countWall
+    }
 
-    fun viewFromId(id: Int) {
-        for ((index, post) in posts.withIndex()) {
-            if (post.id == id) {
-             println(posts[index])
-            }
-            else{
-                println("Нет совпадений")
+    class Likes(var likeCount: Int = 0) {
+        companion object {
+            fun like(postLike: Post): Int {
+                for ((index, post) in posts.withIndex()) {
+                    if (postLike == post && post.likes.likeCount <= 0) {
+                        post.likes.likeCount++
+                        posts[index] = post.copy()
+                        return posts[index].likes.likeCount
+                    } else if (postLike == post) {
+                        post.likes.likeCount--
+                        posts[index] = post.copy()
+                        return posts[index].likes.likeCount
+                    }
+
+                }
+                return postLike.likes.likeCount
             }
         }
     }
 
-    object Likes {
+    class View(var viewCount: Int = 0) {
 
-
-        fun like(post: Post): Post {
-
-
-            if (post.likes <= 0) {
-                post.likes += 1
-            } else {
-                post.likes -= 1
+        companion object {
+            fun view(post: Post): Int {
+                post.view.viewCount++
+                return post.view.viewCount
             }
-
-            return post
         }
     }
 }
-
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,12 @@
 fun main() {
-    val post = Post(0, 2, "Привет", 3, 0)
-    val post1 = Post(1, 2, "Пока", 3, 0)
+    val post = Post(0, text = "Привет")
+    val post1 = Post(2,text = "Пока")
     WallService.add(post)
-    WallService.viewFromId(1)
-    println("Запись добавлена. ID записи: " + post.id)
-    WallService.Likes.like(post)
-    println("Лайк поставлен. Количество лайков: " + post.likes)
-    WallService.Likes.like(post)
-    println("Лайк убран. Количество лайков: " + post.likes)
-    println(WallService.update(post1))
-    WallService.viewFromId(1)
+    WallService.add(post1)
+
+
+
+
 
 
 }
